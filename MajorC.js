474980aa1754a0a9053ebe80cddf4694fbf5e944
Pekka Astala <pekka.astala@sysart.fi>
2018-03-23 23:01:39 +0200

    First version

---
+++ MajorC.js
@@ -0,0 +1,19 @@
+const chordsC = ['Cmaj', 'Dm', 'Em', 'F', 'G', 'Am', 'B°']
+const notesC = [ 'C', 'D', 'E', 'F', 'G', 'A', 'B' ]
+
+const buildNote = position => {
+    if (position > 7) {
+        return {
+            octave: 4 + (position % 7 === 0 ? Math.floor(position / 7) - 1 : Math.floor(position / 7)),
+            step: notesC[position % 7 - 1]
+        }
+    }
+    return {
+        octave: 4,
+        step: notesC[position - 1]
+    }
+};
+
+const notes = degree => [buildNote(degree), buildNote(degree + 2), buildNote(degree + 4)];
+
+module.exports = { notes };
---
+++ app.js
@@ -0,0 +1,11 @@
+const ChordProgressionFactory = require('./ChordProgressionFactory');
+const MajorScale = require('./MajorC');
+const MusicXMLOutput = require('./MusicXMLOutput');
+const fs = require('fs');
+
+const chordProgression = ChordProgressionFactory.generate();
+const measures = chordProgression.map(degree => MajorScale.notes(degree));
+const xml = MusicXMLOutput.toXml(measures);
+fs.writeFileSync("./LatestMelody.xml", xml);
+
+
