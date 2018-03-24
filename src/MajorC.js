df264b16229274f85ec92e2f9244d274d8f31fff
Pekka Astala <pekka.astala@sysart.fi>
2018-03-24 13:07:23 +0200

    Switched to space indents

--- src/MajorC.js
+++ src/MajorC.js
@@ -1,13 +1,13 @@
 const notesC = [ 'C', 'D', 'E', 'F', 'G', 'A', 'B' ];
 
 const buildNote = position => {
-	const octaveIncrease = position % 7 === 0 && position !== 0 ? Math.floor(position / 7) - 1 : Math.floor(position / 7);
-	const step = position % 7 === 0 && position !== 0 ? 6 : position % 7 - 1;
+  const octaveIncrease = position % 7 === 0 && position !== 0 ? Math.floor(position / 7) - 1 : Math.floor(position / 7);
+  const step = position % 7 === 0 && position !== 0 ? 6 : position % 7 - 1;
 
-	return {
-		octave: 4 + octaveIncrease,
-		step: notesC[step],
-	};
+  return {
+    octave: 4 + octaveIncrease,
+    step: notesC[step],
+  };
 };
 
 const notes = degree => [buildNote(degree), buildNote(degree + 2), buildNote(degree + 4)];
