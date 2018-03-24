1a385d64e6223f9ea6a032f8ae1620a8dc62037e
Pekka Astala <pekka.astala@sysart.fi>
2018-03-24 13:10:26 +0200

    Major chord progression no longer uses 7th degree

--- src/ChordProgression.js
+++ src/ChordProgression.js
@@ -1,12 +1,13 @@
 
 function generateMelodyInMajor () {
-  const startingPoint = [
+  const startingPoint = [ // Alway start with first degree, etc.
     1, null, null, 5,
     1, null, null, 1,
     1, null, null, 5,
     1, null, 5, 1,
   ];
-  return startingPoint.map(value => value ? value : Math.floor(Math.random() * 7) + 1);
+  // Fill free positions with random chords from 1 to 6 (avoiding the diminished 7th)
+  return startingPoint.map(value => value ? value : Math.floor(Math.random() * 6) + 1);
 }
 
 module.exports = { generateMelodyInMajor };
