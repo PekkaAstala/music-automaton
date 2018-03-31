024149fb87bc013febabf9c63b1644b578ef4176
Pekka Astala <pekka.astala@sysart.fi>
2018-03-31 23:39:47 +0300

    Added linting rules, ran linter, fixed line endings

--- src/Scale.js
+++ src/Scale.js
@@ -5,11 +5,11 @@ const Note = require('./Note');
 const Chord = require('./Chord');
 
 const NotesOrder = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
-const MajorSteps = ['2', '2', '1', '2', '2', '2', '1']
-const KeyIds = 
-{'C': 0, 'C#': 1, 'Db': 1, 'D': 2, 'D#': 3,'Eb': 3, 'E': 4, 'E#': 5, 'Fb': 4, 
-'F': 5, 'F#': 6, 'Gb': 6,'G': 7,'G#': 8, 'Ab': 8, 'A': 9, 'A#': 10, 'Bb': 10, 
-'B': 11, 'B#': 0, 'Cb': 11,}
+const MajorSteps = ['2', '2', '1', '2', '2', '2', '1'];
+const KeyIds =
+{'C': 0, 'C#': 1, 'Db': 1, 'D': 2, 'D#': 3,'Eb': 3, 'E': 4, 'E#': 5, 'Fb': 4,
+  'F': 5, 'F#': 6, 'Gb': 6,'G': 7,'G#': 8, 'Ab': 8, 'A': 9, 'A#': 10, 'Bb': 10,
+  'B': 11, 'B#': 0, 'Cb': 11,};
 
 const stepsByMajorScale = {
   'C': [ 'C', 'D', 'E', 'F', 'G', 'A', 'B' ],
@@ -27,19 +27,19 @@ const stepsByMajorScale = {
 };
 
 const stepsByMinorScale = {
-    'C': [ 'C', 'D', 'E', 'F', 'G', 'A', 'B' ],
-    'D': [ 'D', 'E', 'F#', 'G', 'A', 'B', 'C#' ],
-    'E': [ 'E', 'F#', 'G#', 'A', 'B', 'C#', 'D#' ],
-    'F': [ 'F', 'G', 'A', 'Bb', 'C', 'D', 'E' ],
-    'G': [ 'G', 'A', 'B', 'C', 'D', 'E', 'F#' ],
-    'A': [ 'A', 'B', 'C#', 'D', 'E', 'F#', 'G#' ],
-    'B': [ 'B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#' ],
-    'Db': [ 'Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb', 'C' ],
-    'Eb': [ 'Eb', 'F', 'G', 'Ab', 'Bb', 'C' ],
-    'F#': [ 'F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#' ],
-    'Ab': [ 'Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G' ],
-    'Bb': [ 'Bb', 'C', 'D', 'Eb', 'F', 'G', 'A' ]
-  };
+  'C': [ 'C', 'D', 'E', 'F', 'G', 'A', 'B' ],
+  'D': [ 'D', 'E', 'F#', 'G', 'A', 'B', 'C#' ],
+  'E': [ 'E', 'F#', 'G#', 'A', 'B', 'C#', 'D#' ],
+  'F': [ 'F', 'G', 'A', 'Bb', 'C', 'D', 'E' ],
+  'G': [ 'G', 'A', 'B', 'C', 'D', 'E', 'F#' ],
+  'A': [ 'A', 'B', 'C#', 'D', 'E', 'F#', 'G#' ],
+  'B': [ 'B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#' ],
+  'Db': [ 'Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb', 'C' ],
+  'Eb': [ 'Eb', 'F', 'G', 'Ab', 'Bb', 'C' ],
+  'F#': [ 'F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#' ],
+  'Ab': [ 'Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G' ],
+  'Bb': [ 'Bb', 'C', 'D', 'Eb', 'F', 'G', 'A' ]
+};
 
 const circularArray = (array, index) => {
   const calculatedIndex = ((index + 1) % array.length) - 1;
@@ -52,15 +52,15 @@ class Scale {
     //case if scale entry doesn't match, find enharmonic degree
     this.mode = mode;
     if (this.mode == this.mode == 'I'){
-        //calls a function that cares about creating the proper scale array
-        //this.steps = KeyIds[firstStep];
+      //calls a function that cares about creating the proper scale array
+      //this.steps = KeyIds[firstStep];
     }
 
     else if (this.mode == 'VI'){
-        this.steps = stepsByMinorScale[firstStep];
+      this.steps = stepsByMinorScale[firstStep];
     }
-    
-    
+
+
   }
 
   getStep(degree) {
@@ -132,30 +132,30 @@ class Scale {
     return firstNote.isBelow(lastNote) ? rangeAscending(firstNote, lastNote) : rangeDescending(firstNote, lastNote);
   }
 
- /**
+  /**
   * This function creates an array of strings containing the notes of a given scale (firstStep) accordingly to to the
   * mode (mode) given as parameter.
-  * 
+  *
   * @param {string} firstStep       first note of the scale (tonic or name of the key)
   * @param {mode} string            mode of the scale represented by a roman number from I to VII (I = Major VI = minor)
   * @return {array}                 returns an array of strings representing the degrees of the scale.
   */
- 
+
   getMajorScale(firstStep, mode) {
 
     // 1- create an array with the proper note names const NotesOrder = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
     scaleArray = [];
     while (i < 7){
-        scaleArray.push(NotesOrder[this.firstStep] + i)
-        i++;
+      scaleArray.push(NotesOrder[this.firstStep] + i);
+      i++;
     }
 
     i = 0; // necessary?
 
     while (i < scaleArray.length()) {
-        scaleArray.push(NotesOrder[this.firstStep] + i)
-        i++;
+      scaleArray.push(NotesOrder[this.firstStep] + i);
+      i++;
     }
 
     i = 0;
@@ -164,40 +164,38 @@ class Scale {
     //if < or > replace with appropriate note -- chose one with the appropriate number that match the name of the notes
 
     while (i < scaleArray.length()) {
-       
-        if (KeyID[scaleArray[i+1]] - KeyID[scaleArray[i]] < MajorSteps[i] ){
-        
-            noteName = scaleArray[i+1];
-            noteKeyID = KeyID[scaleArray[i]];
-            // imcomplete
 
-        }
+      if (KeyID[scaleArray[i+1]] - KeyID[scaleArray[i]] < MajorSteps[i] ){
 
-        if (KeyID[scaleArray[i+1]] - KeyID[scaleArray[i]] < MajorSteps[i] ){
-        
-            noteName = scaleArray[i+1];
-            noteKeyID = KeyID[scaleArray[i]];
-            // imcomplete
+        noteName = scaleArray[i+1];
+        noteKeyID = KeyID[scaleArray[i]];
+        // imcomplete
 
-        }
+      }
 
-        if (KeyID[scaleArray[i+1]] - KeyID[scaleArray[i]] < MajorSteps[i] ){
-        
-            noteName = scaleArray[i+1];
-            noteKeyID = KeyID[scaleArray[i]];
-            // imcomplete
+      if (KeyID[scaleArray[i+1]] - KeyID[scaleArray[i]] < MajorSteps[i] ){
 
-        }
+        noteName = scaleArray[i+1];
+        noteKeyID = KeyID[scaleArray[i]];
+        // imcomplete
 
-        i++;
-        
+      }
+
+      if (KeyID[scaleArray[i+1]] - KeyID[scaleArray[i]] < MajorSteps[i] ){
+
+        noteName = scaleArray[i+1];
+        noteKeyID = KeyID[scaleArray[i]];
+        // imcomplete
+
+      }
+
+      i++;
+
     }
 
     return scaleArray;
   }
 
-
-  
 
 }
 
